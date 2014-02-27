d6b161a817cb17652f58a9cce2089da9a0ae9a23
Maximilian Fischer <mafi@inagile.org>
2014-02-27 18:15:06 +0100

    Add spin.js instead of low res .gif for spinner

--- js/lightbox.js
+++ js/lightbox.js
@@ -58,6 +58,8 @@ Licensed under the Creative Commons Attribution 2.5 License - http://creativecom
     Lightbox.prototype.build = function() {
       var _this = this;
       $("<div id='lightboxOverlay' class='lightboxOverlay'></div><div id='lightbox' class='lightbox'><div class='lb-outerContainer'><div class='lb-container'><img class='lb-image' src='' /><div class='lb-nav'><a class='lb-prev' href='' ></a><a class='lb-next' href='' ></a></div><div class='lb-loader'><a class='lb-cancel'></a></div></div></div><div class='lb-dataContainer'><div class='lb-data'><div class='lb-details'><span class='lb-caption'></span><span class='lb-number'></span></div><div class='lb-closeContainer'><a class='lb-close'></a></div></div></div></div>").appendTo($('body'));
+      var spinner = new Spinner().spin();
+      $('.lb-loader').append(spinner.el);
       this.$lightbox = $('#lightbox');
       this.$overlay = $('#lightboxOverlay');
       this.$outerContainer = this.$lightbox.find('.lb-outerContainer');
